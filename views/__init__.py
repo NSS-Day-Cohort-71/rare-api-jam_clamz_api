0cf173836381c6d6de0b0910241478e103fa1977
Michael Galo <michael@michaelthinks.co>
2024-08-28 15:57:05 -0500

    do_PUT method created for the edit_post fn. Not succesfully returning category_id to client

--- views/__init__.py
+++ views/__init__.py
@@ -1,4 +1,9 @@
 from .user import login_user, create_user, get_user
-from .posts import get_all_posts, create_post, get_posts_by_user_id, get_post_by_id
+from .posts import (
+    get_all_posts,
+    create_post,
+    get_posts_by_user_id,
+    get_post_by_id,
+    edit_post,
+)
 from .category import create_category, get_all_categories, delete_category
-
