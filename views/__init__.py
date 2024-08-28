b6863b73a3783d66bf59c369ae6f42c6323bf25e
Zachary Thacker <zthacker@outlook.com>
2024-08-28 10:27:03 -0500

    added all categories

--- views/__init__.py
+++ views/__init__.py
@@ -1,3 +1,3 @@
 from .user import login_user, create_user, get_user
 from .posts import get_all_posts, create_post
-from .category import create_category
+from .category import create_category, get_all_categories
